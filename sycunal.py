7ebe9ade2192e0c94ae3f1caedbebc5fc4523560
DanD1511 <ddacunha012001@gmail.com>
2023-03-09 13:29:51 -0500

    sycunal 1.0

--- sycunal.py
+++ sycunal.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy; from scipy import signal
 import control
+import bokeh
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource
+from bokeh.models import HoverTool
+from bokeh.plotting import figure
 
 # Transfer Function (tf)
 
@@ -11,9 +16,21 @@ def tf(num, den):
 
 # Step Response
 
-def step(G):
-    t = np.linspace(0, 10, 1000)
-    t, y = control.step_response(G, t)
-    plt.plot(t, y)
-    return plt.show()
+def step(num, den):
 
+    tf1 = signal.TransferFunction(num, den)
+    t, y = signal.step(tf1)
+    source = ColumnDataSource(data=dict(x=t, y=y))
+
+    p = figure(title='Step Response', x_axis_label='Time', y_axis_label='Amplitud', width=1000, height=400)
+
+    p.line(t, y, line_width=2)
+
+    bokeh.plotting.show(p)
+
+
+
+
+
+
+step([1, 2], [1, 2, 3])
