240ddb8b5e42a074652a720692b17bdcd16e420e
DanD1511 <ddacunha012001@gmail.com>
2023-03-09 13:41:54 -0500

    step update with bokeh

--- sycunal.py
+++ sycunal.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy; from scipy import signal
 import control
+import numpy as np
 import bokeh
+from bokeh.io import output_notebook, show
 from bokeh.layouts import column
 from bokeh.models import ColumnDataSource
 from bokeh.models import HoverTool
-from bokeh.plotting import figure, show
+from bokeh.plotting import figure
 
 # Transfer Function (tf)
 
@@ -20,11 +22,14 @@ def step(num, den):
 
     tf1 = signal.TransferFunction(num, den)
     t, y = signal.step(tf1)
-    source = ColumnDataSource(data=dict(x=t, y=y))
+    #source = ColumnDataSource(data=dict(x=t, y=y))
 
     p = figure(title='Step Response', x_axis_label='Time', y_axis_label='Amplitud', width=1000, height=400)
 
     p.line(t, y, line_width=2)
+
+    hover = HoverTool(tooltips=[("Tiempo", "@x"), ("Amplitud", "@y")])
+    p.add_tools(hover)
 
     show(p)
 
