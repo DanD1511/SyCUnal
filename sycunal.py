332b0acab16bafb20f722f3a610bd9aa375e4c57
DanD1511 <ddacunha012001@gmail.com>
2023-05-12 13:32:02 -0500

    rlocus

--- sycunal.py
+++ sycunal.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from control.matlab import *
 import plotly.graph_objects as go
 
-def rlocus_(G):
+def rlocusyc(G):
     # Calcular el lugar de las raíces
     rlist, klist = rlocus(G)
     fig = go.Figure()
